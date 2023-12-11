cf66a2cee4c5809b44ba6c817b0650cd24856ae1
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2023-12-11 18:51:24 +0100

    add: style.css

--- censore.php
+++ censore.php
@@ -9,19 +9,22 @@ $text_lngt = strlen($text);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>PHP Badwords</title>
 </head>
 <body>
-    <h2>Testo che hai inserito:</h2>
-    <?php
-    echo $text;
-    ?>
-    <h3>Il testo contiene <?php echo $text_lngt; ?> lettere.</h3>
-
-    <h2>Testo censurato:</h2>
-    <?php
-    echo $cens_text = str_replace($censita, '***', $text);
-    ?>
-    <h3>Il testo censito contiene <?php echo strlen($cens_text); ?> lettere.</h3>
+    <div>
+        <h2>Testo che hai inserito:</h2>
+        <p> <?php
+        echo $text;
+        ?> </p>
+        <h4>Il testo contiene <span> <?php echo $text_lngt; ?> </span> lettere.</h4>
+        <hr>
+        <h2>Testo censurato:</h2>
+        <p> <?php
+        echo $cens_text = str_replace($censita, '***', $text);
+        ?> </p>
+        <h4>Il testo censito contiene <span> <?php echo strlen($cens_text); ?> </span>lettere.</h4>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,20 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>PHP Badwords</title>
 </head>
 <body>
-    <h1>CENSURATORE DI TESTI</h1>
-    <h2>Inserisci il testo e la parola che vuoi che venga censurata</h2>
+    <div>
+        <h1>CENSURATORE DI TESTI</h1>
+        <h3>Inserisci il testo e la parola che vuoi che venga censurata</h2>
 
-    <form action="censore.php" method="GET">
-        <label for="text">Il tuo testo</label>
-        <input type="text" id="text" name="text">
+        <form action="censore.php" method="GET">
+            <label for="text">Il tuo testo</label>
+            <textarea id="text" name="text" rows="4" cols="50"> </textarea>
 
-        <label for="censita">La parola da censurare</label>
-        <input type="text" id="censita" name="censita">
+            <label for="censita">La parola da censurare</label>
+            <input type="text" id="censita" name="censita">
 
-        <button type="submit">Invia</button>
-    </form>
+            <button type="submit">Invia</button>
+        </form>
+    </div>
 </body>
 </html>
